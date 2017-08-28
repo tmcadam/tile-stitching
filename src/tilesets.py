b68635be96071b145d89d383081363063130cda8
Tom McAdam <mcadam1980@gmail.com>
2017-08-28 16:01:00 +0900

    Cleaned up tilesets

--- src/tilesets.py
+++ src/tilesets.py
@@ -20,36 +20,3 @@ sample1 = TileSet(
     zoom_max=19,
     provider=providers.google
 )
-
-tdc1 = TileSet(
-    name="Tristan de Chuna - Main Island",
-    version="1",
-    description="Tristan de Chuna - Main Island aerial imagery",
-    folder="TDC1_GOOGLE",
-    extents=(-37.037891, -37.217790, -12.370015, -12.202239),
-    zoom_min=13,
-    zoom_max=19,
-    provider=providers.google
-)
-
-tdc2 = TileSet(
-    name="Tristan de Chuna - Inaccessible Island",
-    version="1",
-    folder="TDC2_GOOGLE",
-    description="Tristan de Chuna - Inaccessible Island aerial imagery",
-    extents=(-37.274763, -37.331535, -12.716661, -12.625830),
-    zoom_min=13,
-    zoom_max=19,
-    provider=providers.google
-)
-
-tdc3 = TileSet(
-    name="Tristan de Chuna - Nightingale Islands",
-    version="1",
-    folder="TDC3_GOOGLE",
-    description="Tristan de Chuna - Nightingale Islands aerial imagery",
-    extents=(-37.391934, -37.445445, -12.505140, -12.460771),
-    zoom_min=13,
-    zoom_max=19,
-    provider=providers.google
-)

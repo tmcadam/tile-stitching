f75e4b912c5bedc66121735c64639fbc3b38fe31
Tom McAdam <mcadam1980@gmail.com>
2017-08-28 19:43:38 +0900

    Altered tileset to be chosen from command line argment

--- src/tile.py
+++ src/tile.py
@@ -4,35 +4,33 @@
 #################################################################
 
 import sys
-import tilesets
+from tilesets import TILE_SETS
 import tile_tools
 
 # python tile.py "/home/tmcadam/Desktop/Tiles" download
 
 sys.stdout.write("\n##### Tile Utilities ####\n\n")
 
-OUT_DIR = sys.argv[1]
+t = tile_tools.TileDownloadJob(sys.argv[2], TILE_SETS[sys.argv[1]])
 
-t = tile_tools.TileDownloadJob(OUT_DIR, tilesets.sample1)
-
-if sys.argv[2] == "count_tiles":
+if sys.argv[3] == "count_tiles":
     print "Total tiles in area:\t", (t.counts["exists"] + t.counts["download"])
     print "Already downloaded:\t", t.counts["exists"]
     print "Need downloaded:\t", t.counts["download"]
-elif sys.argv[2] == "download":
+elif sys.argv[3] == "download":
     sys.stdout.write("Tiles within extents: {}\n".format(t.counts["download"] + t.counts["exists"]))
     sys.stdout.write("Tiles already downloaded: {}\n".format(t.counts["exists"]))
     sys.stdout.write("Tiles to download: {}\n\n".format(t.counts["download"]))
     t.get_tiles()
-elif sys.argv[2] == "clean_download":
+elif sys.argv[3] == "clean_download":
     sys.stdout.write("Checking downloaded tiles....\n")
     t.clean_download()
-elif sys.argv[2] == "create_viewer":
+elif sys.argv[3] == "create_viewer":
     sys.stdout.write("Creating html viewer....")
     t.write_leaflet_viewer()
-elif sys.argv[2] == "mbtiles":
+elif sys.argv[3] == "mbtiles":
     t.write_mbtiles()
-elif sys.argv[2] == "geotiff":
+elif sys.argv[3] == "geotiff":
     sys.stdout.write("Creating GeoTiff....\n")
     ## This is used to create a geotiff, mainly for ArcMap users.
     s = tile_tools.TileStitchJob(t)
--- src/tilesets.py
+++ src/tilesets.py
@@ -12,13 +12,23 @@
 from tile_tools import TileSet
 import providers
 
-sample1 = TileSet(
-    name="East Jetty - Stanley",
-    version="1",
-    description="Sample download of East Jetty - Port Stanley",
-    folder="SAMPLE_1",
-    extents=(-51.690410, -51.695710, -57.861578, -57.847802),
-    zoom_min=13,
-    zoom_max=19,
-    provider=providers.google
-)
+TILE_SETS = {
+    "sample1": TileSet(
+        name="East Jetty - Stanley",
+        version="1",
+        description="Sample download of East Jetty - Port Stanley",
+        folder="SAMPLE_1",
+        extents=(-51.690410, -51.695710, -57.861578, -57.847802),
+        zoom_min=13,
+        zoom_max=19,
+        provider=providers.google),
+    "sample2": TileSet(
+        name="Monash Uni = Melbourne",
+        version="1",
+        description="Sample download of Monash uni - Melbourne",
+        folder="SAMPLE_2",
+        extents=(-37.90932, -37.91144, 145.12877, 145.13168),
+        zoom_min=13,
+        zoom_max=19,
+        provider=providers.google)
+}

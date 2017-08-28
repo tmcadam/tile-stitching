6e419f5a0d0d3fbc674bb01e82de08194c169860
Tom McAdam <mcadam1980@gmail.com>
2017-08-28 12:12:23 +0900

    Changed to running from command line arguments

--- tile_tools/tile.py
+++ tile_tools/tile.py
@@ -2,22 +2,32 @@ import sys
 import tilesets
 import tile_tools
 
+# python tile.py "/home/tmcadam/Desktop/Tiles" download
 
 sys.stdout.write("\n##### Tile Utilities ####\n\n")
 
-OUT_DIR = "/home/tom/Desktop/Tiles"
+OUT_DIR = sys.argv[1]
 
-t = tile_tools.TileDownloadJob(OUT_DIR, tilesets.tdc1)
+t = tile_tools.TileDownloadJob(OUT_DIR, tilesets.sample1)
 
-sys.stdout.write("Tiles within extents: {}\n".format(t.counts["download"] + t.counts["exists"]))
-sys.stdout.write("Tiles already downloaded: {}\n".format(t.counts["exists"]))
-sys.stdout.write("Tiles to download: {}\n\n".format(t.counts["download"]))
+if sys.argv[2] == "download":
+    sys.stdout.write("Tiles within extents: {}\n".format(t.counts["download"] + t.counts["exists"]))
+    sys.stdout.write("Tiles already downloaded: {}\n".format(t.counts["exists"]))
+    sys.stdout.write("Tiles to download: {}\n\n".format(t.counts["download"]))
+    t.get_tiles()
+elif sys.argv[2] == "clean_download":
+    sys.stdout.write("Checking downloaded tiles....\n")
+    t.clean_download()
+elif sys.argv[2] == "create_viewer":
+    sys.stdout.write("Creating html viewer....")
+    t.write_leaflet_viewer()
+elif sys.argv[2] == "mbtiles":
+    sys.stdout.write("Converting to MBTiles format....")
+    t.write_mbtiles()
+else:
+    print("ERROR: Command not found\n")
 
-t.get_tiles()
-#t.write_leaflet_viewer()
-#t.write_mbtiles()
-
-sys.stdout.write("\n##### Downloads Finished ####\n\n")
+sys.stdout.write("\n##### Finished ####\n\n")
 
 # s = TileStitchJob(t, 16)
 # s.stitch()

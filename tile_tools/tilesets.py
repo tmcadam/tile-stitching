c3794746fa24a67067e841d8efee7b1a2a9c378b
Tom McAdam <mcadam1980@gmail.com>
2017-08-28 12:28:05 +0900

    Added a small sample tileset for testing

--- tile_tools/tilesets.py
+++ tile_tools/tilesets.py
@@ -1,17 +1,30 @@
-# Top, Bottom, Left, Right
+#############################################################################
+#
+#   Use this file to define new jobs
+#   Extents in WGS84 -> (Top, Bottom, Left, Right)
+#   e.g.    monash_extents = (-37.90932, -37.91144, 145.12877, 145.13168)
+#           sample_extents = (-51.690410, -51.695710, -57.861578, -57.847802)
+#
+##############################################################################
+
 from tile_tools import TileSet
 import providers
 
-# extents = (Top, Bottom, Left, Right)
-#yogya_extents = (-7.74042, -7.84275, 110.31925, 110.41628)
-#rio_extents = (-22.744461, -23.092303, -43.836728, -43.089037)
-#monash_extents = (-37.90932, -37.91144, 145.12877, 145.13168)
-
+sample1 = TileSet(
+    name="East Jetty - Stanley",
+    version="1",
+    description="Sample download of East Jetty - Port Stanley",
+    folder="SAMPLE_1",
+    extents=(-51.690410, -51.695710, -57.861578, -57.847802),
+    zoom_min=13,
+    zoom_max=19,
+    provider=providers.google
+)
 
 tdc1 = TileSet(
-    name='Tristan de Chuna - Main Island',
-    version='1',
-    description='Tristan de Chuna - Main Island aerial imagery',
+    name="Tristan de Chuna - Main Island",
+    version="1",
+    description="Tristan de Chuna - Main Island aerial imagery",
     folder="TDC1_GOOGLE",
     extents=(-37.037891, -37.217790, -12.370015, -12.202239),
     zoom_min=13,
@@ -20,10 +33,10 @@ tdc1 = TileSet(
 )
 
 tdc2 = TileSet(
-    name='Tristan de Chuna - Inaccessible Island',
-    version='1',
+    name="Tristan de Chuna - Inaccessible Island",
+    version="1",
     folder="TDC2_GOOGLE",
-    description='Tristan de Chuna - Inaccessible Island aerial imagery',
+    description="Tristan de Chuna - Inaccessible Island aerial imagery",
     extents=(-37.274763, -37.331535, -12.716661, -12.625830),
     zoom_min=13,
     zoom_max=19,
@@ -31,10 +44,10 @@ tdc2 = TileSet(
 )
 
 tdc3 = TileSet(
-    name='Tristan de Chuna - Nightingale Islands',
-    version='1',
+    name="Tristan de Chuna - Nightingale Islands",
+    version="1",
     folder="TDC3_GOOGLE",
-    description='Tristan de Chuna - Nightingale Islands aerial imagery',
+    description="Tristan de Chuna - Nightingale Islands aerial imagery",
     extents=(-37.391934, -37.445445, -12.505140, -12.460771),
     zoom_min=13,
     zoom_max=19,

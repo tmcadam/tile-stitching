275729b1fc2e061707f18806d624ec90cfc01424
Tom McAdam <mcadam1980@gmail.com>
2017-08-30 15:57:21 +0900

    Added a larger sample, useful for testing proxies

--- src/tilesets.py
+++ src/tilesets.py
@@ -5,8 +5,6 @@
 
 #   Use this file to define new jobs
 #   Extents in WGS84 -> (Top, Bottom, Left, Right)
-#   e.g.    monash_extents = (-37.90932, -37.91144, 145.12877, 145.13168)
-#           sample_extents = (-51.690410, -51.695710, -57.861578, -57.847802)
 
 
 from tile_tools import TileSet
@@ -28,6 +26,15 @@ TILE_SETS = {
         description="Sample download of Monash uni - Melbourne",
         folder="SAMPLE_2",
         extents=(-37.90932, -37.91144, 145.12877, 145.13168),
+        zoom_min=13,
+        zoom_max=19,
+        provider=providers.google),
+    "sample3": TileSet(
+        name='Big Sample Area',
+        version='1',
+        description='A large sample area',
+        folder="SAMPLE_3",
+        extents=(-37.037891, -37.217790, -12.370015, -12.202239),
         zoom_min=13,
         zoom_max=19,
         provider=providers.google)
